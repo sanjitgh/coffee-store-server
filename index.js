9ffaaebf02d8581a42ea1c0aecd18c5abb10abb6
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2024-12-01 04:57:50 +0600

    edit

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    const userCollection = client.db('coffeeDB').collection('users')
 
     // host data in mongoDB
     app.post('/coffee', async (req, res) => {
@@ -82,6 +83,42 @@ async function run() {
       res.send(result)
     })
 
+
+    // users related apis
+
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      console.log('creating new user', newUser);
+
+      const result = await userCollection.insertOne(newUser);
+      res.send(result)
+    })
+
+    app.patch('/users', async (req, res) => {
+      const email = req.body.email;
+      const filter = { email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body.lastSignInTime
+        }
+      }
+
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
